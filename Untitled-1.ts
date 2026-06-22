9094cc0beaf8d1fac62335d329845d0f575595a6
Pratibha Aggarwal <pratibhaaggarwal32@gmail.com>
2026-06-22 23:51:40 +0530

    learning typescript 3.0

--- Untitled-1.ts
+++ Untitled-1.ts
@@ -3,3 +3,10 @@ console.log ("hi");
 // function
 function greet() { console.log ("goodmorning"); }
 greet();
+
+// functiom expression
+const greet2 = function () {
+  console.log ("hello");
+}
+
+greet2();
